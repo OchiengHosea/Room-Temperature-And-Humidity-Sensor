deb442d1ff6d61bd3bbe4989a0a4a3b712072b70
OchiengHosea <jrhosea8@gmail.com>
2022-04-13 19:41:52 +0300

    Trying to add mqtt

--- Client.py
+++ Client.py
@@ -1,19 +1,19 @@
 import json
 import paho.mqtt.client as paho
-from email import message
 
 # public static final String hiveMQURL = "3221fc9b1a7e4e76ad7cce10b8489e96.s1.eu.hivemq.cloud";
     # public static final int hiveMQPort = 8883;
     # public static final String hiveCloudUsername = "duke__";
     # public static final String hiveCloudPass = "dukeHiveMQ8";
 
-class Client():
+class Client:
     def __init__(self) -> None:
         self.client = paho.Client()
         self.client.on_connect = self.on_connect
         self.client.username_pw_set("duke__", "dukeHiveMQ8")
         self.client.connect("3221fc9b1a7e4e76ad7cce10b8489e96.s1.eu.hivemq.cloud", 8883)
-        self.client.loop_start()
+        # self.client.loop_start()
+        print("connected", self.client)
 
 
     def on_connect(self, client, data, flags, rc):
@@ -24,4 +24,5 @@ class Client():
             message = data
         else:
             message = json.dumps(data)
+        print("publishing to ", topic)
         self.client.publish(topic, message, qos=1)
--- dht11.py
+++ dht11.py
@@ -2,13 +2,24 @@ import board
 import time
 import adafruit_dht
 import csv
-import Client
+import json
 from datetime import datetime
+import paho.mqtt.client as paho
 
+def on_connect(client, data, flags, rc):
+    print("CONNACK received with code %d" % (rc))
+
+def on_publish(client, data, mid):
+    print(str(mid))
 
 dht_device = adafruit_dht.DHT11(board.D4)
 fieldnames = ['datetime', 'temperature', 'humidity']
-hive_client = Client()
+# hive_client = Client()
+client = paho.Client()
+client.on_connect = on_connect
+client.username_pw_set("duke__", "dukeHiveMQ8")
+client.connect("3221fc9b1a7e4e76ad7cce10b8489e96.s1.eu.hivemq.cloud", 8883)
+client.loop_start()
 
 with open(f'{datetime.now().date()}.csv', 'w') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -24,7 +35,7 @@ while True:
                 'humidity': humidity
                 }
             writer.writerow(data)
-            hive_client.publish_message('smarthouse/duke/temphum', data)
+            client.publish('smarthouse/duke/temphum', json.dumps(data))
         print(f"Temp {temperature}; himidity {humidity}")
     except Exception as e:
         print(f'Fatal Error; {str(e)}')
