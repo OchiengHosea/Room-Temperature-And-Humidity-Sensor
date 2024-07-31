eb66d4cf63511cee6e4900e5cf96c6ffcf39cd7e
OchiengHosea <jrhosea8@gmail.com>
2024-08-01 02:38:54 +0300

    get device IDs from cli

--- dht11.py
+++ dht11.py
@@ -1,4 +1,6 @@
 import os
+import uuid
+import argparse
 import board
 import time
 import adafruit_dht
@@ -15,7 +17,6 @@ MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
 MQTT_HOST = os.getenv('MQTT_HOST')
 MQTT_PUB_TOPIC = os.getenv('MQTT_PUB_TOPIC')
 KAFKA_PUB_TOPIC = os.getenv('KAFKA_PUB_TOPIC')
-DEVICE_UUID = os.getenv('DEVICE_UUID')
 
 def on_connect(client, data, flags, rc):
     print("CONNACK received with code %d" % (rc))
@@ -27,34 +28,51 @@ def on_message(client, userdata, msg):
 def on_publish(client, data, mid):
     print("published", str(mid))
 
-dht_device = adafruit_dht.DHT11(board.D4)
-fieldnames = ['datetime', 'timestamp', 'temperature', 'humidity']
-client = mqtt.Client("rasp4b", True, None, mqtt.MQTTv31)
-client.on_connect = on_connect
-client.on_message = on_message
-client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
-client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-client.connect(MQTT_HOST, 8883, 60)
-client.loop_start()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device_id', type=uuid.UUID, required=True, dest='device_id', help='Add device_id')
+    parser.add_argument('--latitude', type=float, required=False, dest='latitude', help='Latitude of device')
+    parser.add_argument('--longitude', type=float, required=False, dest='longitude', help='Longitude of device')
+    parser.add_argument('--physical_address', type=str, required=False, dest='physical_address', help='Address(Location name) of device')
+    args = parser.parse_args()
 
-with open(f'data/{datetime.now().date()}.csv', 'w') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    
-while True:
-    try:
-        temperature, humidity = dht_device.temperature, dht_device.humidity
-        with open(f'data/{datetime.now().date()}.csv', 'a') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            data = {
-                'datetime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                'timestamp': time.time() * 10000000,
-                'temperature': temperature,
-                'humidity': humidity,
-                'uuid':str(DEVICE_UUID)
-                }
-            writer.writerow(data)
-            client.publish(f'/{MQTT_PUB_TOPIC}', json.dumps(data))
-        print(f"Temp {temperature}; himidity {humidity}")
-    except Exception as e:
-        print(f'Fatal Error; {str(e)}')
-    time.sleep(60)
+    DEVICE_UUID = args.device_id
+    LATITUDE = args.latitude
+    LONGITUDE = args.longitude
+    PHYSICAL_ADDRESS = args.physical_address
+
+    dht_device = adafruit_dht.DHT11(board.D4)
+    fieldnames = ['datetime', 'timestamp', 'temperature', 'humidity']
+    client = mqtt.Client("rasp4b", True, None, mqtt.MQTTv31)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    client.connect(MQTT_HOST, 8883, 60)
+    client.loop_start()
+
+    with open(f'data/{datetime.now().date()}.csv', 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        
+    while True:
+        try:
+            temperature, humidity = dht_device.temperature, dht_device.humidity
+            with open(f'data/{datetime.now().date()}.csv', 'a') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                data = {
+                    'datetime': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    'timestamp': time.time() * 10000000,
+                    'temperature': temperature,
+                    'humidity': humidity,
+                    'latitude': LATITUDE,
+                    'longitude': LONGITUDE,
+                    'physical_address': PHYSICAL_ADDRESS,
+                    'uuid': str(DEVICE_UUID)
+                    }
+                writer.writerow(data)
+                client.publish(f'/{MQTT_PUB_TOPIC}', json.dumps(data))
+            print(f"Temp {temperature}; himidity {humidity}")
+        except Exception as e:
+            print(f'Fatal Error; {str(e)}')
+        time.sleep(60)
+
